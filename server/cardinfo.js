b737a622dd8a467117aee6d2d1a3537c741da293
itsAlaska <smackey85@yahoo.com>
2024-04-21 03:31:51 -0400

    Adds cardinfo.js and the types of all skills

---
+++ server/cardinfo.js
@@ -0,0 +1,793 @@
+const types = [
+            //      0       1        2       3       4
+                "Arcane", "Area", "Aura", "Blast", "Bomb", 
+            //     5        6        7          8           9 
+                "Burst", "Chain", "Chaos", "Electric", "Empowering", 
+            //    10       11        12     13       14
+                "Fire", "Frontal", "Holy", "Ice", "Lasting",  
+            //      15        16         17         18           19
+                "Magical", "Missile", "Nature", "Physical", "Projectile",  
+            //      20         21       22       23        24
+                "Seasonal", "Shadow", "Slam", "Static", "Summon", 
+            //     25       26
+                "Swing", "Thrust"  
+            ]
+
+const arcanePower = {
+    "Types": [types[0], types[9], types[14], types[1], types[15]]
+}
+
+const magicMissiles = {
+    "Types": [types[0], types[16], types[11], types[5], types[15]]
+}
+
+const arcaneDisc = {
+    "Types": [types[0], types[16], types[11], types[15]]
+}
+
+const arcaneSparks = {
+    "Types": [types[0], types[16], types[5], types[15]]
+}
+
+const arcaneBeam = {
+    "Types": [types[0], types[14], types[11], types[15]]
+}
+
+const arcaneRift = {
+    "Types": [types[0], types[14], types[1], types[15]]
+}
+
+const arcaneShield = {
+    "Types": [types[0], types[9], types[14], types[15]]
+}
+
+const arcaneOrb = {
+    "Types": [types[0], types[16], types[14], types[11], types[15]]
+}
+
+const arcaneConjuration = {
+    "Types": [types[0], types[1], types[15]]
+}
+
+const arcaneExplosion = {
+    "Types": [types[0], types[1], types[5], types[15]]
+}
+
+const arcaneMissiles = {
+    "Types": [types[0], types[16], types[5], types[15]]
+}
+
+const bloodlust = {
+    "Types": [types[7], types[9], types[14], types[1], types[18]]
+}
+
+const chaosBolt = {
+    "Types": [types[7], types[16], types[11], types[15], types[18]]
+}
+
+const chaosGolem = {
+    "Types": [types[7], types[24], types[15], types[18]]
+}
+
+const deathClaw = {
+    "Types": [types[7], types[1], types[5], types[15]]
+}
+
+const chaosWave = {
+    "Types": [types[7], types[1], types[5], types[15]]
+}
+
+const chaosTotem = {
+    "Types": [types[7], types[23], types[24], types[15]]
+}
+
+const manifestationOfChaos = {
+    "Types": [types[7], types[16], types[5], types[15]]
+}
+
+const unchainedChaos = {
+    "Types": [types[7], types[16], types[6], types[15]]
+}
+
+const auraOfChaos = {
+    "Types": [types[7], types[14], types[2], types[15]]
+}
+
+const chaoticMissile = {
+    "Types": [types[7], types[16], types[11], types[15]]
+}
+
+const seekingChaos = {
+    "Types": [types[7], types[16], types[5], types[15]]
+}
+
+const chaoticInferno = {
+    "Types": [types[7], types[14], types[11], types[15]]
+}
+
+const chaosEruption = {
+    "Types": [types[7], types[16], types[11], types[5], types[18]]
+}
+
+const frostBolt = {
+    "Types": [types[13], types[16], types[11], types[15], types[18]]
+}
+
+const glacier = {
+    "Types": [types[13], types[1], types[15]]
+}
+
+const iceVortex = {
+    "Types": [types[13], types[14], types[1], types[15]]
+}
+
+const blizzard = {
+    "Types": [types[13], types[14], types[1], types[15]]
+}
+
+const severeCold = {
+    "Types": [types[13], types[16], types[11], types[15]]
+}
+
+const surpriseGift = {
+    "Types": [types[13], types[4], types[14], types[23], types[18], types[20]]
+}
+
+const avalanche = {
+    "Types": [types[13], types[4], types[1], types[11], types[18], types[20]]
+}
+
+const SummonHelperElves = {
+    "Types": [types[13], types[24], types[15], types[18], types[20]]
+}
+
+const bestialWrath = {
+    "Types": [types[17], types[24], types[18]]
+}
+
+const cobraTotem = {
+    "Types": [types[17], types[23], types[24], types[18]]
+}
+
+const contagion = {
+    "Types": [types[17], types[1], types[5], types[15], types[18]]
+}
+
+const corrosion = {
+    "Types": [types[17], types[14], types[2], types[15]]
+}
+
+const corrosiveSpear = {
+    "Types": [types[17], types[16], types[11], types[18]]
+}
+
+const infectedShot = {
+    "Types": [types[17], types[16], types[5], types[15]]
+}
+
+const poisonBolt = {
+    "Types": [types[17], types[16], types[11], types[15], types[18]]
+}
+
+const poisonCloud = {
+    "Types": [types[17], types[14], types[1], types[18]]
+}
+
+const pathOfDecay = {
+    "Types": [types[17], types[14], types[1], types[15]]
+}
+
+const plagueRats = {
+    "Types": [types[17], types[24], types[15], types[18]]
+}
+
+const putrefy = {
+    "Types": [types[17], types[1], types[5], types[15]]
+}
+
+const putridHounds = {
+    "Types": [types[17], types[24], types[15], types[18]]
+}
+
+const swampBoar = {
+    "Types": [types[17], types[24], types[15], types[18]]
+}
+
+const warTiger = {
+    "Types": [types[17], types[24], types[18]]
+}
+
+const fireBolt = {
+    "Types": [types[10], types[16], types[11], types[15], types[18]]
+}
+
+const meteor = {
+    "Types": [types[10], types[1], types[15]]
+}
+
+const flameWave = {
+    "Types": [types[10], types[1], types[5], types[15]]
+}
+
+const fireWall = {
+    "Types": [types[10], types[14], types[1], types[15]]
+}
+
+const meteorShower = {
+    "Types": [types[10], types[1], types[5], types[15]]
+}
+
+const conflagrate = {
+    "Types": [types[10], types[1], types[5], types[15]]
+}
+
+const lavaShield = {
+    "Types": [types[10], types[14], types[2], types[15]]
+}
+
+const fireWalk = {
+    "Types": [types[10], types[14], types[1], types[15]]
+}
+
+const flamethrower = {
+    "Types": [types[10], types[14], types[11], types[15]]
+}
+
+const firePillar = {
+    "Types": [types[10], types[1], types[5], types[15]]
+}
+
+const fireSlash = {
+    "Types": [types[10], types[16], types[11], types[15]]
+}
+
+const combustion = {
+    "Types": [types[10], types[14], types[2], types[15]]
+}
+
+const fieryMissiles = {
+    "Types": [types[10], types[16], types[5], types[15]]
+}
+
+const rayOfFire = {
+    "Types": [types[10], types[1], types[5], types[15]]
+}
+
+const heatExplosion = {
+    "Types": [types[10], types[1], types[5], types[15]]
+}
+
+const orbsOfDestruction = {
+    "Types": [types[10], types[16], types[11], types[15]]
+}
+
+const volcanoEruption = {
+    "Types": [types[10], types[23], types[24], types[15]]
+}
+
+const lightningBolt = {
+    "Types": [types[8], types[16], types[15], types[18]]
+}
+
+const thunderStrike = {
+    "Types": [types[8], types[1], types[15]]
+}
+
+const chainLightning = {
+    "Types": [types[8], types[6], types[5], types[15]]
+}
+
+const tornado = {
+    "Types": [types[8], types[14], types[1], types[15]]
+}
+
+const callLightning = {
+    "Types": [types[8], types[1], types[15]]
+}
+
+const lightningBeam = {
+    "Types": [types[8], types[14], types[11], types[15]]
+}
+
+const summonInfantry = {
+    "Types": [types[8], types[24], types[15], types[18]]
+}            
+
+const shadowBolt = {
+    "Types": [types[21], types[16], types[15], types[18]]
+}
+
+const shadowSpike = {
+    "Types": [types[21], types[16], types[5], types[18]]
+}
+
+// This is called 'Void' in game, just cannot name a variable void alone
+const voidSkill = {
+    "Types": [types[21], types[14], types[1], types[15]]
+}
+
+const deathVortex = {
+    "Types": [types[21], types[14], types[1], types[15]]
+}
+
+const shadowOrb = {
+    "Types": [types[21], types[16], types[14], types[11], types[15]]
+}
+
+const shadowStep = {
+    "Types": [types[21], types[1], types[18]]
+}
+
+const skeletalMage = {
+    "Types": [types[21], types[24], types[15], types[18]]
+}
+
+const skeletalArchers = {
+    "Types": [types[21], types[24], types[15], types[18]]
+}
+
+const unholyBolt = {
+    "Types": [types[21], types[16], types[11], types[15], types[18]]
+}
+
+const shadowNova = {
+    "Types": [types[21], types[1], types[5], types[15]]
+}
+
+const rayOfDoom = {
+    "Types": [types[21], types[14], types[2], types[15]]
+}
+
+const unholyAura = {
+    "Types": [types[21], types[14], types[2], types[15]]
+}
+
+const skeletalWarriors = {
+    "Types": [types[21], types[24], types[15], types[18]]
+}
+
+const lightBeam = {
+    "Types": [types[12], types[14], types[11], types[15]]
+}
+
+const might = {
+    "Types": [types[12], types[9], types[14], types[1], types[18]]
+}
+
+const smite = {
+    "Types": [types[12], types[9], types[14], types[1], types[18]]
+}
+
+const beaconOfLight = {
+    "Types": [types[12], types[14], types[2], types[18]]
+}
+
+const exorcism = {
+    "Types": [types[12], types[1], types[5], types[18]]
+}
+
+const holyFire = {
+    "Types": [types[12], types[16], types[11], types[5], types[18]]
+}
+
+const hammerOfJustice = {
+    "Types": [types[12], types[25], types[16], types[11], types[18]]
+}
+
+const penance = {
+    "Types": [types[12], types[16], types[6], types[18]]
+}
+
+const sacredOrb = {
+    "Types": [types[12], types[16], types[11], types[15]]
+}
+
+const celestialRetribution = {
+    "Types": [types[12], types[16], types[11], types[15]]
+}
+
+const bladestorm = {
+    "Types": [types[25], types[14], types[1], types[18]]
+}
+
+const heroicStrike = {
+    "Types": [types[12], types[25], types[11], types[18]]
+}
+
+const subdue = {
+    "Types": [types[25], types[11], types[18]]
+}
+
+const titansCleaver = {
+    "Types": [types[25], types[11], types[18]]
+}
+
+const scentOfBlood = {
+    "Types": [types[25], types[9], types[14], types[1], types[18]]
+}
+
+const savageStrike = {
+    "Types": [types[25], types[11], types[18]]
+}
+
+const whirlwind = {
+    "Types": [types[25], types[1], types[18]]
+}
+
+const doubleSlash = {
+    "Types": [types[25], types[11], types[18]]
+}
+
+const throwAxe = {
+    "Types": [types[25], types[16], types[11], types[18]]
+}
+
+const arcaneBlade = {
+    "Types": [types[0], types[25], types[11], types[15], types[18]]
+}
+
+const slash = {
+    "Types": [types[0], types[25], types[11], types[18]]
+}
+
+const uppercut = {
+    "Types": [types[25], types[11], types[18]]
+}
+
+const arcaneSlash = {
+    "Types": [types[0], types[25], types[11], types[15], types[18]]
+}
+
+const arcaneOverload = {
+    "Types": [types[0], types[25], types[1], types[15], types[18]]
+}
+
+const templarsVerdict = {
+    "Types": [types[12], types[25], types[1], types[18]]
+}
+
+const bladesOfLight = {
+    "Types": [types[12], types[25], types[11], types[18]]
+}
+
+const venomousStrike = {
+    "Types": [types[17], types[25], types[11], types[18]]
+}
+
+const festeringStrike = {
+    "Types": [types[17], types[25], types[1], types[15], types[18]]
+}
+
+const shadowGrasp = {
+    "Types": [types[21], types[25], types[1], types[15], types[18]]
+}
+
+const obliterate = {
+    "Types": [types[21], types[25], types[11], types[15], types[18]]
+}
+
+const reap = {
+    "Types": [types[21], types[25], types[11], types[18]]
+}
+
+const thunderingSlash = {
+    "Types": [types[8], types[25], types[11], types[18]]
+}
+
+const frozenBlade = {
+    "Types": [types[13], types[25], types[16], types[11], types[18]]
+}
+
+const heartrendingStrike = {
+    "Types": [types[25], types[1], types[11], types[18], types[20]]
+}
+
+const ravagingStrike = {
+    "Types": [types[25], types[1], types[11], types[18]]
+}
+
+const carnage = {
+    "Types": [types[25], types[16], types[1], types[5], types[18]]
+}
+
+const flurry = {
+    "Types": [types[26], types[14], types[1], types[18]]
+}
+
+const quickStrike = {
+    "Types": [types[26], types[11], types[18]]
+}
+
+const piercingShout = {
+    "Types": [types[26], types[9], types[14], types[1], types[18]]
+}
+
+const arcaneAssault = {
+    "Types": [types[0], types[26], types[11], types[15], types[18]]
+}
+
+const radiantLight = {
+    "Types": [types[0], types[26], types[14], types[1], types[18]]
+}
+
+const twinDaggers = {
+    "Types": [types[26], types[11], types[18]]
+}
+
+const backstab = {
+    "Types": [types[26], types[11], types[18]]
+}
+
+const rupture = {
+    "Types": [types[26], types[11], types[18]]
+}
+
+const sinisterStrike = {
+    "Types": [types[26], types[11], types[18]]
+}
+
+const eviscerate = {
+    "Types": [types[26], types[11], types[18]]
+}
+
+const intimidatingShout = {
+    "Types": [types[26], types[9], types[14], types[1], types[18]]
+}
+
+const shieldWall = {
+    "Types": [types[26], types[9], types[14], types[1], types[18]]
+}
+
+const stormOfSpears = {
+    "Types": [types[26], types[16], types[14], types[18]]
+}
+
+const thrust = {
+    "Types": [types[26], types[11], types[18]]
+}
+
+const shieldBash = {
+    "Types": [types[26], types[11], types[18]]
+}
+
+const jupitersSpear = {
+    "Types": [types[8], types[26], types[11], types[18]]
+}
+
+const arcticAssault = {
+    "Types": [types[13], types[26], types[11], types[18]]
+}
+            
+const sugarRush = {
+    "Types": [types[26], types[9], types[14], types[18], types[20]]
+}
+
+const bodySlam = {
+    "Types": [types[22], types[11], types[18]]
+}
+
+const demolish = {
+    "Types": [types[22], types[16], types[5], types[18]]
+}
+
+const shockwave = {
+    "Types": [types[22], types[1], types[11], types[5], types[18]]
+}
+
+const shockwaveTotem = {
+    "Types": [types[22], types[23], types[24], types[18]]
+}
+
+const thunderClap = {
+    "Types": [types[8], types[22], types[11], types[18]]
+}
+
+const stomp = {
+    "Types": [types[22], types[1], types[5], types[18]]
+}
+
+const frozenWarhammer = {
+    "Types": [types[13], types[22], types[1], types[15]]
+}
+
+const arcaneMace = {
+    "Types": [types[0], types[22], types[1], types[11], types[15]]
+}
+
+const wildStrike = {
+    "Types": [types[17], types[22], types[11], types[18]]
+}
+
+const purgingSlam = {
+    "Types": [types[17], types[22], types[11], types[18]]
+}
+
+const brutalSlam = {
+    "Types": [types[22], types[1], types[11], types[18]]
+}
+
+const caveBear = {
+    "Types": [types[17], types[22], types[24], types[18]]
+}
+
+const bladedChakram = {
+    "Types": [types[19], types[16], types[11], types[18]]
+}
+
+const buildBallista = {
+    "Types": [types[19], types[23], types[24], types[18]]
+}
+
+const chromaticBolt = {
+    "Types": [types[0], types[7], types[13], types[17], types[10], 
+            types[8], types[21], types[12], types[19], types[16], 
+            types[11], types[15], types[18]]
+}
+
+const fragShot = {
+    "Types": [types[19], types[16], types[11], types[19]]
+}
+
+const stormOfArrows = {
+    "Types": [types[19], types[16], types[14], types[19]]
+}
+
+const splitShot = {
+    "Types": [types[19], types[16], types[11], types[18]]
+}
+
+const sniperShot = {
+    "Types": [types[19], types[16], types[11], types[18]]
+}
+
+const ricochetShot = {
+    "Types": [types[19], types[16], types[11], types[18]]
+}
+
+const shoot = {
+    "Types": [types[19], types[16], types[11], types[18]]
+}
+
+const rainOfArrows = {
+    "Types": [types[19], types[14], types[1], types[18]]
+}
+
+const spreadShot = {
+    "Types": [types[19], types[16], types[11], types[5], types[18]]
+}
+
+const obscureArrow = {
+    "Types": [types[21], types[19], types[16], types[11], types[15], types[18]]
+}
+
+const heartbreakOrbs = {
+    "Types": [types[19], types[16], types[11], types[18], types[20]]
+}
+
+const camorsArrow = {
+    "Types": [types[19], types[1], types[18], types[20]]
+}
+
+const eggShooter = {
+    "Types": [types[19], types[16], types[11], types[18], types[20]]
+}
+
+const acidRain = {
+    "Types": [types[17], types[4], types[1], types[11], types[18]]
+}
+
+const bombBarrage = {
+    "Types": [types[4], types[16], types[11], types[5], types[18]]
+}
+
+const chaosBomb = {
+    "Types": [types[7], types[4], types[11], types[15], types[18]]
+}
+
+const clusterBomb = {
+    "Types": [types[4], types[1], types[11], types[5], types[18]]
+}
+
+const explosiveTrap = {
+    "Types": [types[4], types[14], types[23], types[18]]
+}
+
+const mineField = {
+    "Types": [types[4], types[14], types[23], types[18]]
+}
+
+const poisonBomb = {
+    "Types": [types[17], types[4], types[14], types[1], types[18]]
+}
+
+const shrapnelBomb = {
+   "Types": [types[4], types[1], types[11], types[5], types[18]] 
+}
+
+const suctionBomb = {
+    "Types": [types[4], types[1], types[11], types[18]]
+}
+
+const weakeningGrenade = {
+    "Types": [types[4], types[1], types[5], types[18]]
+}
+
+const bearTrap = {
+    "Types": [types[4], types[14], types[23], types[18]]
+}
+
+const shrapnelMine = {
+    "Types": [types[4], types[14], types[23], types[18]]
+}
+
+const mortarShot = {
+    "Types": [types[4], types[1], types[11], types[5], types[18]]
+}
+
+const bombardment = {
+    "Types": [types[4], types[1], types[5], types[18]]
+}
+
+const disengage = {
+    "Types": [types[4], types[1], types[5], types[18]]
+}
+
+const voidTrap = {
+    "Types": [types[21], types[4], types[14], types[23], types[18]]
+}
+
+const smokeBomb = {
+    "Types": [types[21], types[4], types[14], types[11], types[18]]
+}
+
+const debilitatingBomb = {
+    "Types": [types[4], types[16], types[11], types[18]]
+}
+
+const fanOfBombs = {
+    "Types": [types[25], types[4], types[1], types[5], types[18]]
+}
+        
+const frenziedBunnies = {
+    "Types": [types[7], types[4], types[24], types[18], types[20]]
+}
+
+const explosiveShot = {
+    "Types": [types[3], types[16], types[11], types[18]]
+}
+
+const shrapnelShot = {
+    "Types": [types[3], types[16], types[11], types[18]]
+}
+
+const napalmBlast = {
+    "Types": [types[3], types[1], types[11], types[18]]
+}
+
+const powerBlast = {
+    "Types": [types[3], types[11], types[5], types[18]]
+}
+
+const freezingBlow = {
+    "Types": [types[13], types[3], types[1], types[11], types[15]]
+}
+
+const ancientCurse = {
+    "Types": [types[21], types[3], types[1], types[11], types[18]]
+}
+
+const buckshot = {
+    "Types": [types[3], types[11], types[5], types[18]]
+}
+
+const overchargedStrike = {
+    "Types": [types[8], types[3], types[1], types[11], types[18]]
+}
+
+const preciseShot = {
+    "Types": [types[19], types[3], types[16], types[11], types[18]]
+}
+
+const threshingBlast = {
+    "Types": [types[3], types[11], types[5], types[18]]
+}
